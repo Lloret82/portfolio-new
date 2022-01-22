3109624776e90246e4ed71e35c5f729e139ca792
lloret82 <adbmanchester@gmail.com>
2022-01-22 12:48:32 +0000

    added file download

---
+++ studio/schemas/file.js
@@ -0,0 +1,11 @@
+export default {
+      title: 'Manuscript',
+      name: 'manuscript',
+      type: 'file',
+      fields: [
+            {
+                  name: 'description',
+                  type: 'string',
+                  title: 'Description'
+            }]
+}
