3a715675c485ea76e62f09cad8c3e6b0756aa3d6
lloret82 <adbmanchester@gmail.com>
2022-01-23 18:38:20 +0000

    download button

--- src/components/Cv.js
+++ src/components/Cv.js
@@ -23,10 +23,13 @@ const Cv = () => {
       // console.log(cv.cvURL)
 
       return (
-
-            <div>
-                  {cv ? <a href={cv.cvURL} className="text-5xl text-black font-bold cursive leading-none lg:leading-snug home-name" rel="noreferrer">Click here for my CV</a> : ""}
-                  {/* <pre>{JSON.stringify({ cv })}</pre> */}
+            <div class="p-2 flex flex-col justify-center">
+                  <div class="mx-auto mt-6">
+                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold inline-flex border border-blue-700 rounded ">
+                              {cv ? <a href={cv.cvURL} className=" text-black font-bold p-4 leading-none home-name" id="dl" rel="noreferrer">Download</a> : ""}
+                              {/* <pre>{JSON.stringify({ cv })}</pre> */}
+                        </button>
+                  </div>
             </div>
 
       )
