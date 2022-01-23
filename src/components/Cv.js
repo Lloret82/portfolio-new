6c4b01496826e09ea0be790e326f351caaea64ab
lloret82 <adbmanchester@gmail.com>
2022-01-23 13:00:42 +0000

    rel=noreferrer added

--- src/components/Cv.js
+++ src/components/Cv.js
@@ -27,7 +27,7 @@ export default function Cv() {
       console.log(cv.cvURL)
       return (
             <div>
-                  <a href={cv.cvURL} target='_blank' className="text-5xl text-black font-bold cursive leading-none lg:leading-snug home-name">Click here for my CV</a>
+                  <a href={cv.cvURL} target='_blank' className="text-5xl text-black font-bold cursive leading-none lg:leading-snug home-name" rel="noreferrer">Click here for my CV</a>
                   {/* <pre>{JSON.stringify({ cv })}</pre> */}
             </div>
 
